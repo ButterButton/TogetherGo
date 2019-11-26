ba53b4c11326da78a08e2f517885e03c4794c63a
unknown <tim126359@gmail.com>
2019-11-26 22:58:30 +0800

    Commit from home

--- MongoDB_API.py
+++ MongoDB_API.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import Response
+from flask import request
 from flask_cors import CORS
 import pymongo
 import urllib.parse
@@ -7,6 +8,10 @@ from pprint import pprint
 from pymongo import MongoClient
 from bson.json_util import dumps
 import configparser
+import json
+import datetime
+import pytz
+import time
 
 app = Flask(__name__)
 CORS(app)
@@ -15,12 +20,27 @@ CORS(app)
 def index():
     return "Welcome"
     
-@app.route("/POST/<key>/<value>", methods=["POST"])
+@app.route("/GET/Products/<key>/<value>", methods=["GET"])
 def SerachByKey(key, value):
-    collection = connect_mongo()
-    cursor = collection.find({key:value})
+    db = connect_mongo()
+    cursor = db.Products.find({key:value})
     data = [d for d in cursor]
     return Response(dumps(data, ensure_ascii=False, indent=4), mimetype='text/json')
+
+@app.route("/POST/Order/<JsonValue>", methods=["POST"])
+def InsertByJson(JsonValue):
+    db = connect_mongo()
+    # Result = db.Order.insert(json.loads(JsonValue))
+    print(datetime.datetime.now(pytz.timezone("Asia/Taipei")))
+    # twtime = pytz.timezone("Asia/Taipei")
+    # print(datetime.datetime.now())
+    # Result = db.Order.insert({"Date":twtime})
+    # data = [d for d in db.Order.find({"_id":Result})]
+    # if(len(data) < 1):
+    #     return Response(dumps({"Result":"Fail","Data":data}, ensure_ascii=False, indent=4), mimetype='text/json')
+
+    # return Response(dumps({"Result":"Success","Data":data}, ensure_ascii=False, indent=4), mimetype='text/json')
+    return 0
 
 def connect_mongo():
     # collection.stats
@@ -43,10 +63,12 @@ def connect_mongo():
     ConnectString = "mongodb://%s:%s@%s:%s/TogetherGo"%(ConfigFile["DB_TogetherGo"]["USER"],ConfigFile["DB_TogetherGo"]["PWD"],ConfigFile["DB_TogetherGo"]["HOST"], ConfigFile["DB_TogetherGo"]["PORT"])
     client = MongoClient(ConnectString)
     db = client.TogetherGo   
-    collection = db.Products
+    # collection = db.Products
 
-    return collection
+    return db
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
