574a2137fe5d48d99128696e0ffe6f6ae4507163
ButterButton <tim126359@gmail.com>
2019-11-27 18:08:31 +0800

    Test TimeZone

--- MongoDB_API.py
+++ MongoDB_API.py
@@ -20,27 +20,29 @@ CORS(app)
 def index():
     return "Welcome"
     
-@app.route("/GET/Products/<key>/<value>", methods=["GET"])
-def SerachByKey(key, value):
+@app.route("/GET/Products/No/<value>", methods=["GET"])
+def SerachByKey(value):
     db = connect_mongo()
-    cursor = db.Products.find({key:value})
+    cursor = db.Products.find({"No":value})
     data = [d for d in cursor]
     return Response(dumps(data, ensure_ascii=False, indent=4), mimetype='text/json')
 
 @app.route("/POST/Order/<JsonValue>", methods=["POST"])
 def InsertByJson(JsonValue):
     db = connect_mongo()
+    twtime = pytz.utc.localize(datetime.datetime.now()) + datetime.timedelta(hours=8)
+    afterload = json.loads(JsonValue)
+    afterload["Test"] = "87"
+    print(afterload)
     # Result = db.Order.insert(json.loads(JsonValue))
-    print(datetime.datetime.now(pytz.timezone("Asia/Taipei")))
-    # twtime = pytz.timezone("Asia/Taipei")
-    # print(datetime.datetime.now())
-    # Result = db.Order.insert({"Date":twtime})
+    # Result = db.Order.insert({"Date":test1time})
+    # Result = db.Order.insert_many([{"Date":test1time},{"Date":test2time},{"Date":test3time},{"Date":testtimestrTodatetime}])
     # data = [d for d in db.Order.find({"_id":Result})]
     # if(len(data) < 1):
     #     return Response(dumps({"Result":"Fail","Data":data}, ensure_ascii=False, indent=4), mimetype='text/json')
 
     # return Response(dumps({"Result":"Success","Data":data}, ensure_ascii=False, indent=4), mimetype='text/json')
-    return 0
+    return "HI"
 
 def connect_mongo():
     # collection.stats
